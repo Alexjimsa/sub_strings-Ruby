ca998c90d7297a5ad918618b64001fadd938edde
Alexjimsa <alexjimsa10@gmail.com>
2024-09-10 17:22:32 +0200

    The method works

--- app.rb
+++ app.rb
@@ -7,13 +7,24 @@ example_dictionary = ["below","down","go","going","horn","how","howdy",
 
 def substrings(input, dictionary)
   #binding.pry
+  # Splitting the "input" string into an [array]
   word_list = input.split(" ")
   result = []
+  substring_count = {}
+  # Using each to loop through every item from [word_list] (every individual word)
   word_list.each do |word|
-    result.push(dictionary.select { |dict_word| word.downcase.include?(dict_word) })
+    result = dictionary.select { |dict_word| word.downcase.include?(dict_word) }
+    # Using each to modify the hash {substring_count}, wich is what the method should return
+    result.flatten.each do |sub|
+      if substring_count[sub] == nil
+        substring_count[sub] = 1
+      else
+        substring_count[sub] = substring_count[sub] + 1
+      end
+    end
   end
-  p result
+  return substring_count
 end
 
 
-substrings("Howdy dear friend!", example_dictionary)
+p substrings("Howdy dear friend! I'm super exited of you getting a birthday party.", example_dictionary)
